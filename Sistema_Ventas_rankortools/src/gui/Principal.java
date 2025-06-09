366500eac7db556f2de37cc923c527ed309f46bf
shern <shern@Yume>
2025-06-09 01:08:08 -0500

    Gui - Principal y pestañas enlazadas

--- Sistema_Ventas_rankortools/src/gui/Principal.java
+++ Sistema_Ventas_rankortools/src/gui/Principal.java
@@ -2,15 +2,21 @@ package gui;
 
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Color;
 
 public class Principal extends JFrame implements ActionListener {
 
@@ -21,15 +27,16 @@ public class Principal extends JFrame implements ActionListener {
 	private JMenu mnVentas;
 	private JMenu mnAcercaDe;
 	private JMenu mnAyuda;
-	private JMenuItem mntmNewMenuItem;
-	private JMenuItem mntmNewMenuItem_1;
-	private JMenuItem mntmNewMenuItem_2;
-	private JMenuItem mntmNewMenuItem_3;
+	private JMenuItem mntmTienda;
+	private JMenuItem mntmIntegrantes;
+	private JMenuItem mntmVentasDia;
+	private JMenuItem mntmSalir;
 	private JMenuItem mntnConsultar;
 	private JMenuItem mntnModificar;
 	private JMenuItem mntmListar;
-	private JMenuItem mntmNewMenuItem_4;
-	private JMenuItem mntmNewMenuItem_5;
+	private JMenuItem mntmIngresarSolicitud;
+	private JMenuItem mntmReporteVentas;
+	private JLabel lblFondo;
 
 	/**
 	 * Launch the application.
@@ -53,7 +60,7 @@ public class Principal extends JFrame implements ActionListener {
 	public Principal() {
 		setTitle("Rankor Perú");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 670, 417);
+		setBounds(100, 100, 866, 435);
 		
 		menuBar = new JMenuBar();
 		menuBar.setFont(new Font("Segoe UI", Font.BOLD, 16));
@@ -63,10 +70,10 @@ public class Principal extends JFrame implements ActionListener {
 		mnAyuda.setFont(new Font("Segoe UI", Font.BOLD, 16));
 		menuBar.add(mnAyuda);
 		
-		mntmNewMenuItem_3 = new JMenuItem("Salir");
-		mntmNewMenuItem_3.addActionListener(this);
-		mntmNewMenuItem_3.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		mnAyuda.add(mntmNewMenuItem_3);
+		mntmSalir = new JMenuItem("Salir");
+		mntmSalir.addActionListener(this);
+		mntmSalir.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+		mnAyuda.add(mntmSalir);
 		
 		mnProductos = new JMenu("Productos");
 		mnProductos.setFont(new Font("Segoe UI", Font.BOLD, 16));
@@ -91,55 +98,59 @@ public class Principal extends JFrame implements ActionListener {
 		mnVentas.setFont(new Font("Segoe UI", Font.BOLD, 16));
 		menuBar.add(mnVentas);
 		
-		mntmNewMenuItem_4 = new JMenuItem("Ingresar solicitud");
-		mntmNewMenuItem_4.addActionListener(this);
-		mntmNewMenuItem_4.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		mnVentas.add(mntmNewMenuItem_4);
+		mntmIngresarSolicitud = new JMenuItem("Ingresar solicitud");
+		mntmIngresarSolicitud.addActionListener(this);
+		mntmIngresarSolicitud.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+		mnVentas.add(mntmIngresarSolicitud);
 		
-		mntmNewMenuItem_5 = new JMenuItem("Reporte de ventas");
-		mntmNewMenuItem_5.addActionListener(this);
-		mntmNewMenuItem_5.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		mnVentas.add(mntmNewMenuItem_5);
+		mntmReporteVentas = new JMenuItem("Reporte de ventas");
+		mntmReporteVentas.addActionListener(this);
+		mntmReporteVentas.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+		mnVentas.add(mntmReporteVentas);
 		
 		mnAcercaDe = new JMenu("Acerca de");
 		mnAcercaDe.setFont(new Font("Segoe UI", Font.BOLD, 16));
 		menuBar.add(mnAcercaDe);
 		
-		mntmNewMenuItem = new JMenuItem("Tienda");
-		mntmNewMenuItem.addActionListener(this);
-		mntmNewMenuItem.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		mnAcercaDe.add(mntmNewMenuItem);
+		mntmTienda = new JMenuItem("Tienda");
+		mntmTienda.addActionListener(this);
+		mntmTienda.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+		mnAcercaDe.add(mntmTienda);
 		
-		mntmNewMenuItem_1 = new JMenuItem("Integrantes");
-		mntmNewMenuItem_1.addActionListener(this);
-		mntmNewMenuItem_1.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		mnAcercaDe.add(mntmNewMenuItem_1);
+		mntmIntegrantes = new JMenuItem("Integrantes");
+		mntmIntegrantes.addActionListener(this);
+		mntmIntegrantes.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+		mnAcercaDe.add(mntmIntegrantes);
 		
-		mntmNewMenuItem_2 = new JMenuItem("Ventas del dia");
-		mntmNewMenuItem_2.addActionListener(this);
-		mntmNewMenuItem_2.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-		mnAcercaDe.add(mntmNewMenuItem_2);
+		mntmVentasDia = new JMenuItem("Ventas del dia");
+		mntmVentasDia.addActionListener(this);
+		mntmVentasDia.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+		mnAcercaDe.add(mntmVentasDia);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		ImageIcon icon = new ImageIcon("imagenes/principal.png");
+		Image img = icon.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
+		lblFondo = new JLabel(new ImageIcon(img));
+		lblFondo.setBounds(0, 0, 853, 365);
+		contentPane.add(lblFondo);
+
+		/*lblFondo = new JLabel("");
+		lblFondo.setBackground(Color.GRAY);
+		lblFondo.setIcon(new ImageIcon("imagenes/principal.png"));
+		lblFondo.setBounds(-8, -12, 763, 309);
+		contentPane.add(lblFondo);*/
+		
+		setLocationRelativeTo(this);
+		
+		
 	}
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == mntmNewMenuItem_2) {
-			actionPerformedMntmNewMenuItem_2(e);
-		}
-		if (e.getSource() == mntmNewMenuItem_1) {
-			actionPerformedMntmNewMenuItem_1(e);
-		}
-		if (e.getSource() == mntmNewMenuItem) {
-			actionPerformedMntmNewMenuItem(e);
-		}
-		if (e.getSource() == mntmNewMenuItem_5) {
-			actionPerformedMntmNewMenuItem_5(e);
-		}
-		if (e.getSource() == mntmNewMenuItem_4) {
-			actionPerformedMntmNewMenuItem_4(e);
+		if (e.getSource() == mntmSalir) {
+			actionPerformedMntmSalir(e);
 		}
 		if (e.getSource() == mntmListar) {
 			actionPerformedMntmListar(e);
@@ -150,26 +161,67 @@ public class Principal extends JFrame implements ActionListener {
 		if (e.getSource() == mntnConsultar) {
 			actionPerformedMntnConsultar(e);
 		}
-		if (e.getSource() == mntmNewMenuItem_3) {
-			actionPerformedMntmNewMenuItem_3(e);
+		if (e.getSource() == mntmIngresarSolicitud) {
+			actionPerformedMntmIngresarSolicitud(e);
+		}
+		if (e.getSource() == mntmReporteVentas) {
+			actionPerformedMntmReporteVentas(e);
+		}
+		if (e.getSource() == mntmTienda) {
+		actionPerformedMntmTienda(e);
+		}
+		if (e.getSource() == mntmIntegrantes) {
+			actionPerformedMntmIntegrantes(e);
+		}
+		if (e.getSource() == mntmVentasDia) {
+			actionPerformedMntmVentasDia(e);
 		}
 	}
-	protected void actionPerformedMntmNewMenuItem_3(ActionEvent e) {
+	protected void actionPerformedMntmSalir(ActionEvent e) {
+		int respuesta;		
+	    respuesta = JOptionPane.showConfirmDialog(this, "¿Desea Salir del programa?" , "IMPORTANTE", 
+		JOptionPane.YES_NO_OPTION, 2);
+	    if(respuesta == 0)
+		System.exit(0);
 	}
 	protected void actionPerformedMntnConsultar(ActionEvent e) {
+		DialogConsultar dC = new DialogConsultar();
+		dC.setLocationRelativeTo(dC);
+		dC.setVisible(true);
 	}
 	protected void actionPerformedMntnModificar(ActionEvent e) {
+		DialogModificar dM = new DialogModificar();
+		dM.setLocationRelativeTo(dM);
+		dM.setVisible(true);
 	}
 	protected void actionPerformedMntmListar(ActionEvent e) {
+		DialogListar dL = new DialogListar();
+		dL.setLocationRelativeTo(dL);
+		dL.setVisible(true);
 	}
-	protected void actionPerformedMntmNewMenuItem_4(ActionEvent e) {
+	protected void actionPerformedMntmIngresarSolicitud(ActionEvent e) {
+		DialogIngresarSolicitud dIngresarSol = new DialogIngresarSolicitud();
+		dIngresarSol.setLocationRelativeTo(dIngresarSol);
+		dIngresarSol.setVisible(true);
 	}
-	protected void actionPerformedMntmNewMenuItem_5(ActionEvent e) {
+	protected void actionPerformedMntmReporteVentas(ActionEvent e) {
+		DialogReporteVentas dReporteV = new DialogReporteVentas();
+		dReporteV.setLocationRelativeTo(dReporteV);
+		dReporteV.setVisible(true);
 	}
-	protected void actionPerformedMntmNewMenuItem(ActionEvent e) {
+	protected void actionPerformedMntmTienda(ActionEvent e) {
+		DialogTienda dTienda = new DialogTienda();
+		dTienda.setLocationRelativeTo(dTienda);
+		dTienda.setVisible(true);
 	}
-	protected void actionPerformedMntmNewMenuItem_1(ActionEvent e) {
+	protected void actionPerformedMntmIntegrantes(ActionEvent e) {
+		DialogIntegrantes dIntegrantes = new DialogIntegrantes();
+		dIntegrantes.setLocationRelativeTo(dIntegrantes);
+		dIntegrantes.setVisible(true);
 	}
-	protected void actionPerformedMntmNewMenuItem_2(ActionEvent e) {
+	protected void actionPerformedMntmVentasDia(ActionEvent e) {
+		DialogVentasDia dVentaDia = new DialogVentasDia();
+		dVentaDia.setLocationRelativeTo(dVentaDia);
+		dVentaDia.setVisible(true);
 	}
 }
