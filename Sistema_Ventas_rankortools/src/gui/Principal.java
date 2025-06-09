c18b73466df29052a4017812669dbf08ca15ad48
User <User@DESKTOP-F6GMRIR>
2025-06-09 17:29:43 -0500

    getgetgtge

--- Sistema_Ventas_rankortools/src/gui/Principal.java
+++ Sistema_Ventas_rankortools/src/gui/Principal.java
@@ -6,7 +6,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class Principal extends JFrame {
+public class Principal extends JFrame 
+{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
