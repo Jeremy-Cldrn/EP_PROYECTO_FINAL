e855927085d607e16f88ee0cbad86e3338414fc7
Jeremy-Cldrn <jeremycldrn.chavez02@gmail.com>
2025-06-28 17:28:42 -0400

    modificacion

--- Sistema_Ventas_rankortools/src/gui/FrmReporteStock.java
+++ Sistema_Ventas_rankortools/src/gui/FrmReporteStock.java
@@ -1,24 +1,15 @@
 package gui;
 
-import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.Font;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 public class FrmReporteStock extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JLabel blVentaDeproductos;
-	private JScrollPane scr;
-	private JTextArea txtS;
 
 	/**
 	 * Launch the application.
@@ -36,6 +27,7 @@ public class FrmReporteStock extends JFrame {
 		});
 	}
 
+
 	/**
 	 * Create the frame.
 	 */
@@ -47,22 +39,6 @@ public class FrmReporteStock extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		blVentaDeproductos = new JLabel("HISTORIAL DE VENTAS");
-		blVentaDeproductos.setBounds(-1, -1, 554, 34);
-		blVentaDeproductos.setOpaque(true);
-		blVentaDeproductos.setHorizontalAlignment(SwingConstants.CENTER);
-		blVentaDeproductos.setForeground(Color.WHITE);
-		blVentaDeproductos.setFont(new Font("Tahoma", Font.BOLD, 20));
-		blVentaDeproductos.setBackground(Color.BLACK);
-		contentPane.add(blVentaDeproductos);
-		
-		scr = new JScrollPane();
-		scr.setBounds(15, 42, 524, 596);
-		contentPane.add(scr);
-		
-		txtS = new JTextArea();
-		txtS.setEditable(false);
-		scr.setViewportView(txtS);
 	}
 
 }
