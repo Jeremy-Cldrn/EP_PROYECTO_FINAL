9e2d535a0f969d3b3eb4f755a3b8ddb1b7f6903f
Jeremy-Cldrn <jeremycldrn.chavez02@gmail.com>
2025-06-29 22:21:11 -0400

    clientes

--- Sistema_Ventas_rankortools/src/arreglos/ArreglosClientes.java
+++ Sistema_Ventas_rankortools/src/arreglos/ArreglosClientes.java
@@ -1,19 +1,27 @@
 package arreglos;
 
+
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+
 
 import clases.Clientes;
 
 public class ArreglosClientes {
+	
 	private ArrayList<Clientes> listaClientes;
 	
 	public ArreglosClientes() {
 		listaClientes = new ArrayList<Clientes>();
+		cargarClientes();
 	}
 	
 	public void adicionar(Clientes c) {
 		listaClientes.add(c);
+		grabarClientes();
 	}
 	
 	public int tamanio() {
@@ -34,18 +42,50 @@ public class ArreglosClientes {
 	
 	public void eliminar(Clientes c) {
 		listaClientes.remove(c);
+		grabarClientes();
 	}
 	
-	/*private void cargarClientes() {
+	private void cargarClientes() {
 		try {
 			BufferedReader br;
 			String[] s;
-			String linea, nombres, apellidos, direccionString;
-			int telefono;
-			br= new BufferedReader(br);
+			String linea, nombres, apellidos, direccion;
+			int dni, telefono;
+			br= new BufferedReader(new FileReader("clientes.txt"));
+				while((linea=br.readLine())!=null) {
+					s=linea.split(";");
+					dni = Integer.parseInt(s[0]);
+					nombres = s[1];
+					apellidos = s[2];
+					direccion =  s[3];
+					telefono = Integer.parseInt(s[4]);
+					listaClientes.add(new Clientes(dni, nombres, apellidos, direccion, telefono));
+				}
+				br.close();
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-	}*/
+	}
+	
+	private void grabarClientes() {	
+		try {
+			PrintWriter pw;
+			String linea;
+			Clientes x;
+			pw = new PrintWriter(new FileWriter("clientes.txt"));
+			for (int i = 0; i < tamanio(); i++) {
+				x=obtener(i);
+				linea = x.getDni()+";"+
+						x.getNombres()+";"+
+						x.getApellidos()+";"+
+						x.getDireccion()+";"+
+						x.getTelefono();
+				pw.println(linea);
+			}
+			pw.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 }
 
